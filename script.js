2a60278815bc101e04a27b774aca5fa71be08651
Daphnetatertot <daphnetatertot@gmail.com>
2022-12-25 22:47:01 -0500

    added a few more prompt boxes, and if statments to allow for lower upper and special chars

--- script.js
+++ script.js
@@ -1,68 +1,79 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowercase = "abcdefghijklmnopqrstuvwxyz".split('')
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('')
-var numbers = "0123456789".split('')
-var specialChars = "!#@%^*$".split('')
-var passwordLegnthPrompt = window.prompt("How long would you like your password to be?")
-var hasLowercase = window.confirm("Would you like lowercase letters?")
+var lowercase = "abcdefghijklmnopqrstuvwxyz".split("");
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var numbers = "0123456789".split("");
+var specialChars = "!#@%^*$".split("");
+var passwordLegnthPrompt = window.prompt(
+  "How long would you like your password to be?"
+);
 
-
+//Window promtpts to confirm what criteria to include
+var hasLowercase = window.confirm("Would you like lowercase letters?");
+var hasSpecial = window.confirm('Would you like to include special characters (@#$%^)');
 
 function generatePassword() {
-  console.log("Generating password...")
-  var result =[]
-  var passwordLegnth = Number.parseInt(passwordLegnthPrompt)
-  var charArray = []
-  var guaranteedChars = []
-  
+  console.log("Generating password...");
+  var result = [];
+  var passwordLegnth = Number.parseInt(passwordLegnthPrompt);
+  var charArray = [];
+  var guaranteedChars = [];
 
   //sets the min & max characters allowed
   if (passwordLegnth < 8 || passwordLegnth > 128) {
-    console.log(passwordLegnth)
-    window.alert("Your password must be at least 8 characters, and less than 128 characters!")
-    return null
+    console.log(passwordLegnth);
+    window.alert(
+      "Your password must be at least 8 characters, and less than 128 characters!"
+    );
+    return null;
   }
-
+// if statements to add all criteria (upper lower, special, and numbers)
   if (hasLowercase) {
-    charArray = charArray.concat(lowercase)
-    guaranteedChars.push(getRandomIndex(lowercase))
-
+    charArray = charArray.concat(lowercase);
+    guaranteedChars.push(getRandomIndex(lowercase));
   }
 
   if (upperCase) {
-    charArray = charArray.concat(upperCase) 
-    guaranteedChars.push(getRandomIndex(upperCase))
-   
+    charArray = charArray.concat(upperCase);
+    guaranteedChars.push(getRandomIndex(upperCase));
+  }
 
+  if (specialChars) {
+    charArray = charArray.concat(specialChars);
+    guaranteedChars.push(getRandomIndex(specialChars));
+  }
+
+  if (numbers) {
+    charArray = charArray.concat(numbers);
+    guaranteedChars.push(getRandomIndex(numbers));
   }
 
   var i = 0;
   var j = 0;
   while (i < passwordLegnth) {
-    var possibleChar = getRandomIndex(charArray)
-    result.push(possibleChar)
-    console.log(result)
-    i++
+    var possibleChar = getRandomIndex(charArray);
+    result.push(possibleChar);
+    console.log(result);
+    i++;
   }
   while (j < guaranteedChars.length) {
-    console.log("result index, guaranteed chars index", result[j], guaranteedChars[j])
-    result[j] = guaranteedChars[j]
-    j++
-    
+    console.log(
+      "result index, guaranteed chars index",
+      result[j],
+      guaranteedChars[j]
+    );
+    result[j] = guaranteedChars[j];
+    j++;
   }
 
-  return result.join('')
-
-
+  return result.join("");
 }
 // item is an array
 function getRandomIndex(item = []) {
-  var index = Math.floor(Math.random() * item.length)
-  return item[index]
-
+  var index = Math.floor(Math.random() * item.length);
+  return item[index];
 }
-console.log(getRandomIndex())
+
 
 // Write password to the #password input
 function writePassword() {
@@ -70,10 +81,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
